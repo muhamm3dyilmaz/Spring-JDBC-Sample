f45435dfbce7df87ab26268cb01b34f64cf79ecb
muhamm3dyilmaz <mylmz2101@gmail.com>
2024-09-16 14:50:07 +0300

    data.sql created at /resources

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into student (studentNo, name, mark) values (101, 'Yilmaz', 85);
+insert into student (studentNo, name, mark) values (102, 'Gokhan', 52);
+insert into student (studentNo, name, mark) values (103, 'Hakan', 69);
